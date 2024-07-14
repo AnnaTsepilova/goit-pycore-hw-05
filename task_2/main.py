15933e42b23a2bbaeeb395603699282168899734
Anna Tsepilova <virgo.tsav@gmail.com>
2024-07-14 22:16:34 +0300

    HW3 - ready

--- task_2/main.py
+++ task_2/main.py
@@ -1,10 +1,20 @@
+import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    pass
+    '''
+    Generator return float from text
+    '''
+    for word in text.split(' '):
+        if re.match(r"\d", word):
+            yield float(word)
 
 def sum_profit(text: str, func: Callable):
-    pass
+    total_amount = 0
+    for amount in func(text):
+        total_amount += amount
+
+    return total_amount
 
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
