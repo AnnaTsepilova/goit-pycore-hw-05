acc0166cb220fca84ca451d31a66685cf3d4dd27
Anna Tsepilova <virgo.tsav@gmail.com>
2024-07-14 20:14:00 +0300

    Calculate stats with collection

--- task_3/main.py
+++ task_3/main.py
@@ -1,15 +1,14 @@
 import sys
 import re
+import collections
 from pathlib import Path
-
-##message_stats = {}
 
 def parse_log_line(line: str) -> dict:
     '''
     Parse log line in format: DATE LEVEL MESSAGE
     '''
     parsed = re.split(r"^(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s(\w+)\s(.*)$", line)
-    return {'date': parsed[1], 'level': parsed[2], 'message': parsed[3]}
+    return {'date': parsed[1], 'level': parsed[2].upper(), 'message': parsed[3]}
 
 def load_logs(file_path: str) -> list:
     p = Path(file_path)
@@ -19,9 +18,6 @@ def load_logs(file_path: str) -> list:
 
     with open(file_path, "r", encoding="utf-8") as fh:
         lines = [el.strip() for el in fh.readlines()]
-        ## Parse file line by line
-        # for line in fh.readlines():
-        #     parse_log_line(line.strip())
 
     ## Handle empty file
     if len(lines) == 0:
@@ -38,13 +34,7 @@ def filter_logs_by_level(logs: list, level: str) -> list:
     return list(filter(lambda log: log['level'] == level, logs))
 
 def count_logs_by_level(logs: list) -> dict:
-    message_stats = {}
-    for log in logs:
-        if not message_stats.get(log['level']):
-            message_stats[log['level']] = 1
-        else:
-            message_stats[log['level']] += 1
-
+    message_stats = dict(collections.Counter(map(lambda log: log['level'], logs)))
     return message_stats
 
 def display_log_counts(counts: dict):
@@ -55,7 +45,10 @@ def display_log_counts(counts: dict):
 
 def main():
     if len(sys.argv) > 1:
-        raw_lines = load_logs(sys.argv[1])
+        log_path = sys.argv[1]
+        log_level = sys.argv[2]
+
+        raw_lines = load_logs(log_path)
 
         parsed_lines = []
         for line in raw_lines:
@@ -64,8 +57,10 @@ def main():
         message_stats = count_logs_by_level(parsed_lines)
         display_log_counts(message_stats)
 
-        filtered = filter_logs_by_level(parsed_lines, "INFO")
-        [print(f"{f['date']} {f['level']} {f['message']}") for f in filtered]
+        if log_level:
+            log_level = log_level.upper().strip()
+            filtered = filter_logs_by_level(parsed_lines, log_level)
+            [print(f"{f['date']} {f['level']} {f['message']}") for f in filtered]
     else:
         print(f"  Usage: main.py LOG_FILE [MESSAGE_TYPE]")
 
